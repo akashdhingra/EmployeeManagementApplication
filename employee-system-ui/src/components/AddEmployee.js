ebca99a63df4270f69a18ccfcf581068a48b3329
Akash Dhingra <akashdhingra1996@gmail.com>
2023-07-14 20:06:26 -0400

    added employeeService file in UI to hit the endpoint

--- employee-system-ui/src/components/AddEmployee.js
+++ employee-system-ui/src/components/AddEmployee.js
@@ -1,4 +1,6 @@
+import e, { response } from 'express';
 import React, { useState } from 'react'
+import EmployeeService from '../service/EmployeeService';
 
 const AddEmployee = () => {
 
@@ -13,6 +15,16 @@ const AddEmployee = () => {
     const handleChange = (e) => {
         const value = e.target.value;
         setEmployee({...employee, [e.target.name]: value});
+    }
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        EmployeeService.saveEmployee(employee).then((response) => {
+            console.log(response);
+
+        }).catch((error) => {
+            console.log(error);
+        })
     }
 
 
@@ -51,7 +63,7 @@ const AddEmployee = () => {
             </div>
 
             <div className='items-centre justify-center h-14 w-full my-4 pt-4'>
-                <button className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>
+                <button onClick={saveEmployee} className='rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6'>
                     Save
                 </button>
                 <button className='rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6'>
