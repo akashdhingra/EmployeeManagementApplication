cbfbaf0323e7a3ddf9d2600f1c630bf094d76184
Akash Dhingra <akashdhingra1996@gmail.com>
2023-07-16 16:59:16 -0400

    Implemented routes in UI

--- employee-system-ui/src/App.js
+++ employee-system-ui/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import AddEmployee from './components/AddEmployee';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from'react-router-dom';
+import EmployeeList from './components/EmployeeList';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar/>
-      <AddEmployee/>
+      <Routes>
+        <Route index element={<EmployeeList />}/>
+        <Route path="/" element={<EmployeeList />}> </Route>
+        <Route path="/employeeList" element={<EmployeeList />}> </Route>
+        <Route path="/addEmployee" element={<AddEmployee />}> </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
